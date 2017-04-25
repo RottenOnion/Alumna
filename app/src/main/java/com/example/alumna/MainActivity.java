80189418e56da1156e751a37fb2bd74d5df62452
RottenOnion <18826077683@163.com>
2017-04-25 23:04:51 +0800

    create respository

---
+++ app/src/main/java/com/example/alumna/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.alumna;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.alumna.adapter.LeftDrawerAdapter;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mLeftRvView;
+    private ArrayList<LeftBean> mLeftDatas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mLeftRvView = (RecyclerView) findViewById(R.id.main_left_recycler);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        /*
+         初始化左侧滑菜单Item
+         */
+        loadLeftDatas();
+        LeftDrawerAdapter leftAdapter = new LeftDrawerAdapter(mLeftDatas);
+        mLeftRvView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        mLeftRvView.setAdapter(leftAdapter);
+    }
+
+    private void loadLeftDatas() {
+        mLeftDatas = new ArrayList<>();
+        LeftBean data = new LeftBean(R.drawable.ic_menu_friend,"朋友圈");
+        mLeftDatas.add(data);
+
+        data = new LeftBean(R.drawable.ic_menu_setting,"设置");
+        mLeftDatas.add(data);
+
+        data = new LeftBean(R.drawable.ic_menu_about,"关于");
+        mLeftDatas.add(data);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/alumna/adapter/LeftDrawerAdapter.java
@@ -0,0 +1,69 @@
+package com.example.alumna.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.alumna.LeftBean;
+import com.example.alumna.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by py on 2017/4/24.
+ */
+
+public class LeftDrawerAdapter extends RecyclerView.Adapter<LeftDrawerAdapter.LeftViewHolder> {
+
+    private static View.OnClickListener mClickListener;
+    private LayoutInflater mInflater;
+    private ArrayList<LeftBean> mDatas;
+
+    public LeftDrawerAdapter(ArrayList<LeftBean> mDatas) {
+        this.mDatas = mDatas;
+    }
+
+    @Override
+    public LeftViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.left_drawer_item,parent,false);
+        return new LeftViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(LeftViewHolder holder, int position) {
+        LeftBean data = mDatas.get(position);
+        holder.imageView.setImageResource(data.getImageId());
+        holder.tittle.setText(data.getTittleText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas == null ? 0 : mDatas.size();
+    }
+
+    public static class LeftViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        LinearLayout view;
+        private ImageView imageView;
+        private TextView tittle;
+
+
+        public LeftViewHolder(View itemView) {
+            super(itemView);
+            view = (LinearLayout) itemView.findViewById(R.id.left_drawer_item);
+            imageView = (ImageView) itemView.findViewById(R.id.left_drawer_image);
+            tittle = (TextView) itemView.findViewById(R.id.left_drawer_tittle);
+
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
