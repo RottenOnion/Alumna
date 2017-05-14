5d292a21c96c8f6bb78748fafd66e8a9e635bc07
RottenOnion <18826077683@163.com>
2017-05-14 10:38:24 +0800

    调整

---
+++ app/src/main/java/com/example/alumna/view/InformActivity.java
@@ -0,0 +1,15 @@
+package com.example.alumna.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alumna.R;
+
+public class InformActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inform);
+    }
+}
